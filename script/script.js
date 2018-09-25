3bb1d9ecdfd75c1b73a21290b28ef2da0f7a8739
Hellen.Brunelli <hellenehugo@gmail.com>
2018-09-25 16:11:18 -0300

    Criação do HTML e CSS do projeto

---
+++ script/script.js
@@ -0,0 +1,14 @@
+const inputText = document
+
+const button = document.querySelector(".btn_composer");
+
+const criarTarefas = document.createElement("div");
+
+
+button.addEventListener("click", function(e){
+    e.preventDefault();
+
+
+
+
+})
